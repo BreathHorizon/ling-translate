990ee97ddedd09e783f15a70183f2d0da38c12eb
Zealmult <lagsnes@lagsn.es>
2026-01-17 17:53:27 +0800

    feat: 添加翻译取消功能并更新版本号至0.0.9
    在ContentApp组件中添加翻译取消功能，通过runId跟踪当前翻译任务
    更新package.json和manifest.json中的版本号至0.0.9

--- src/options/components/About.tsx
+++ src/options/components/About.tsx
@@ -5,7 +5,7 @@ import manifest from '../../manifest.json';
 
 export const About: React.FC = () => {
   const version = manifest.version;
-  const build = 30;
+  const build = 31;
 
   return (
     <div className="space-y-6">
