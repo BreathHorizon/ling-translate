1cf68e9af7a8b47dcfba25a6c57e5824b113866b
Zealmult <lagsnes@lagsn.es>
2026-01-17 17:14:25 +0800

    Bump version to 0.0.8 and update build number
    Updated package version to 0.0.8 in package-lock.json and incremented build number to 30 in About.tsx to reflect the latest release.

--- src/options/components/About.tsx
+++ src/options/components/About.tsx
@@ -5,7 +5,7 @@ import manifest from '../../manifest.json';
 
 export const About: React.FC = () => {
   const version = manifest.version;
-  const build = 29;
+  const build = 30;
 
   return (
     <div className="space-y-6">
